2f4613fea4353c1dd4332908737d81dc1e1e78be
86157 <161619532@qq.com>
2023-10-08 18:10:49 +0800

    测试接口

---
+++ src/main/java/cn/go/buy/ticket/config/SwaggerConfig.java
@@ -0,0 +1,19 @@
+package cn.go.buy.ticket.config;
+
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * @author MrKe
+ * @version 1.0.0
+ * @ClassName SwaggerConfig.java
+ * @Description TODO
+ * @createTime 2023年10月08日 15:18
+ */
+@Configuration
+@EnableSwagger2  //开启Swagger
+public class SwaggerConfig {
+//配置文件不写，执行默认配置
+
+}
+
